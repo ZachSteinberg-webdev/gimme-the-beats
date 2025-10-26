a374a83b4defccd8b82594b9dcf6e8f3d97fd525
Zach Steinberg <Zach@ZachSteinberg.com>
2025-10-26 15:35:09 -0700

    Updated Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,4 @@
 import './Footer.module.css';
 export default function Footer() {
-	return <footer>© 2025 Zach Steinberg — All rights reserved</footer>;
+	return <footer>© 2025 <a href='https://www.zachsteinberg.com/' target='_blank' rel='noreferrer'>Zach Steinberg</a> — All rights reserved</footer>;
 }
